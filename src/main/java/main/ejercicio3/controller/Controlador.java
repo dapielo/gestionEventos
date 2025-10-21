0a4936f5e28f8e1afd2af375b1c06005889fd850
David <149360825+dploure@users.noreply.github.com>
2025-10-21 19:19:36 +0200

    version 2

--- src/main/java/main/ejercicio3/controller/Controlador.java
+++ src/main/java/main/ejercicio3/controller/Controlador.java
@@ -1,26 +1,26 @@
 package main.ejercicio3.controller;
 
-import java.util.Arrays;
-import java.util.List;
-
+import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+import org.springframework.web.bind.annotation.RequestParam;
 import main.ejercicio3.model.Evento;
 
 @Controller
 public class Controlador {
+    private ArrayList<Evento> eventos = new ArrayList<>();
+    private int indice;
 
     @GetMapping("/")
     public String inicio(Model modelo){
-        List<Evento> eventos = Arrays.asList(
-            new Evento(1, "Cumpleaños de un amigo", false),
-            new Evento(2, "Fiesta de disfraces", false),
-            new Evento(3, "Festival en el centro", false),
-            new Evento(4, "Ir al cine", false)
-        );
         modelo.addAttribute("eventos", eventos);
         return "index";
+    }
+
+    @GetMapping("/add")
+    public String añadirEvento(@RequestParam String nombre){
+        eventos.add(new Evento(indice++, nombre, false));
+        return "redirect:/";
     }
 }
